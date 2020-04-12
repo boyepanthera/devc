c983f43c7b0634998c712616e14fdd2c7bb4e056
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-04-12 11:34:48 +0100

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,61 @@
-const covid19ImpactEstimator = (data) => data;
+let data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+}
+
+const toDay = (data) =>{
+    if(data.periodType === 'days'){
+        return data.timeToElapse;
+    } else if (data.periodType==='weeks') {
+        return (data.timeToElapse * 7);
+    } else if (data.periodType ==='months') {
+        return(data.periodType*30)
+    } else {
+        return timeToElapse
+    }
+}
+
+
+const covid19ImpactEstimator = (data) => {
+    
+    let {reportedCases, totalHospitalBeds} = data;
+    let {avgDailyIncomePopulation} = data.region
+
+    let currentlyInfected, infectionsByRequestedTime;
+
+    let impact =  ()=> {
+        currentlyInfected = reportedCases * 10;
+        infectionsByRequestedTime =  currentlyInfected * 2 ** (toDay(data)/3);
+        severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
+        hospitalBedsByRequestedTime = 0.35 * totalHospitalBeds - severeCasesByRequestedTime;
+        casesForICUByRequestedTime = infectionsByRequestedTime * 0.15
+        casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02
+        dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation *avgDailyIncomeInUSD / (toDay(data))
+    }
+
+    let severeImpact = () => {
+        currentlyInfected = reportedCases *50;
+        infectionsByRequestedTime = currentlyInfected * 2 ** (toDay(data) / 3);
+        severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
+        hospitalBedsByRequestedTime = 0.35 * totalHospitalBeds - severeCasesByRequestedTime;
+        casesForICUByRequestedTime = infectionsByRequestedTime * 0.15
+        casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02
+        dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation *avgDailyIncomeInUSD / (toDay(data))
+    }
+
+    return {
+        impact,
+        severeImpact
+    }
+};
 
 export default covid19ImpactEstimator;
