c166352b1b3bc4adcb544fa507bb0ffd20e109bb
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-04-12 17:07:24 +0100

    challenge3 v14

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,16 @@
-const data = {
-  region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: "days",
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-}
+// const data = {
+//   region: {
+//     name: "Africa",
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: "days",
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// }
 
 const toDay = (periodType, timeToElapse) => {
   if (periodType === 'days') {
@@ -22,9 +22,10 @@ const toDay = (periodType, timeToElapse) => {
   if (periodType === 'months') {
     return timeToElapse * 30;
   }
+  return timeToElapse;
 };
 
-const covid19ImpactEstimator = (data) => {  
+const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
 
@@ -38,25 +39,36 @@ const covid19ImpactEstimator = (data) => {
   const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data.region;
 
   impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * 2 ** (toDay(periodType, timeToElapse) / 3));
+  impact.infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * 2
+    ** (toDay(periodType, timeToElapse) / 3));
   impact.severeCasesByRequestedTime = Math.trunc(0.15 * impact.infectionsByRequestedTime);
-  impact.hospitalBedsByRequestedTime = Math.trunc(0.35 * totalHospitalBeds - impact.severeCasesByRequestedTime);
+  impact.hospitalBedsByRequestedTime = Math.trunc(0.35 * totalHospitalBeds
+    - impact.severeCasesByRequestedTime);
   impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.15);
   impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02);
-  impact.dollarsInFlight = Math.trunc((infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD) / toDay(periodType, timeToElapse));
-  
-  severeImpact.currentlyInfected = Math.trunc(reportedCases * 50);
-  severeImpact.infectionsByRequestedTime = Math.trunc(severeImpact.currentlyInfected * 2 ** (toDay(periodType, timeToElapse) / 3));
-  severeImpact.severeCasesByRequestedTime = Math.trunc(0.15 * severeImpact.infectionsByRequestedTime);
-  severeImpact.hospitalBedsByRequestedTime = Math.trunc(0.35 * totalHospitalBeds - severeImpact.severeCasesByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.15);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.02);
-  severeImpact.dollarsInFlight = Math.trunc((severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD) / toDay(periodType, timeToElapse));
+  impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime
+    * avgDailyIncomePopulation * avgDailyIncomeInUSD) / toDay(periodType, timeToElapse));
+
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = Math.trunc(severeImpact.currentlyInfected * 2
+    ** (toDay(periodType, timeToElapse) / 3));
+  severeImpact.severeCasesByRequestedTime = Math.trunc(0.15
+    * severeImpact.infectionsByRequestedTime);
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(0.35 * totalHospitalBeds
+    - severeImpact.severeCasesByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime
+    * 0.15);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(
+    severeImpact.infectionsByRequestedTime * 0.02
+  );
+  severeImpact.dollarsInFlight = Math.trunc((severeImpact.infectionsByRequestedTime
+    * avgDailyIncomePopulation * avgDailyIncomeInUSD) / toDay(periodType, timeToElapse));
+
   return {
     data,
     impact,
     severeImpact
-  }
+  };
 };
 
-export default covid19ImpactEstimator
+export default covid19ImpactEstimator;
